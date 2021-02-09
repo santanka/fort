d1b7467e448e50257b3f33f76dd9bf28a2fdd616
santanka <mkrock1062@gmail.com>
2021-02-09 16:13:09 +0900

    電子Larmor半径に相対論効果付与

--- pds_kai/python_draw/codes/pds_E_kai_plot.py
+++ pds_kai/python_draw/codes/pds_E_kai_plot.py
@@ -8,7 +8,7 @@ Created on Wed Feb  3 16:18:06 2021
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.genfromtxt(r"pds_kai/pds_E_kai/pds_E_kai_5_all.csv", delimiter=',', unpack=True)
+data = np.genfromtxt(r"pds_kai/pds_E_kai_L=15/pds_E_kai_L=15_2_all.csv", delimiter=',', unpack=True)
 
 channel = 6
 #1:静電ポテンシャル, 2:数密度, 3:Alfven速度, 4:圧力, 5:ベータ値, 6:Larmor半径&慣性長
@@ -18,7 +18,7 @@ if (channel == 1):
     x = np.rad2deg(x)
     y = data[3, :]
     fig = plt.figure()
-    plt.rcParams["font.size"] = 25
+    plt.rcParams["font.size"] = 20
     ax = fig.add_subplot(111, xlabel='S← MLT [degree] →N', ylabel='Electrostatic Potential [V]')    
     ax.plot(x, y)
     fig.suptitle('Electrostatic Potential')
@@ -31,7 +31,7 @@ if (channel == 2):
     x = np.rad2deg(x)
     y = data[4:16, :]*1.E-6
     fig = plt.figure()
-    plt.rcParams["font.size"] = 25
+    plt.rcParams["font.size"] = 20
     ax = fig.add_subplot(111, xlabel='S← MLT [degree] →N', ylabel='Number Density [cm^-3]', yscale='log', ylim=(1.E-2, 1.E6))   
     ax.plot(x, y[0, :]+y[5, :], c='orange', label='H+(ionosphere)')
     ax.plot(x, y[1, :]+y[6, :], c='red', label='He+')
@@ -50,7 +50,7 @@ if (channel == 3):
     x = np.rad2deg(x)
     y = data[21, :]
     fig = plt.figure()
-    plt.rcParams["font.size"] = 25
+    plt.rcParams["font.size"] = 20
     ax = fig.add_subplot(111, xlabel='S← MLT [degree] →N', ylabel='Alfven Speed [/(Light Speed)]')
     ax.plot(x, y)
     fig.suptitle('Alfven Speed')
@@ -62,7 +62,7 @@ if (channel == 4):
     x = np.rad2deg(x)
     y = data[70:79]*1.E9
     fig = plt.figure()
-    plt.rcParams["font.size"] = 25
+    plt.rcParams["font.size"] = 20
     ax1 = fig.add_subplot(131, title='perpendicular')
     ax1.set_xlabel('S← MLT [degree] →N')
     ax1.set_ylabel('Pressure [nPa]')
@@ -99,7 +99,7 @@ if (channel == 5):
     y = data[82, :]
     z = data[88, :]
     fig = plt.figure()
-    plt.rcParams["font.size"] = 25
+    plt.rcParams["font.size"] = 20
     ax = fig.add_subplot(111, xlabel='S← MLT [degree] →N', ylabel='beta', yscale='log')
     ax.plot(x, y, c='orange', label='beta')
     ax.plot(x, z, c='dimgrey', label='me/mi', linestyle='-.')
@@ -113,21 +113,27 @@ if (channel == 6):
     x = np.rad2deg(x)
     y = data[89:93, :]
     BB = data[2][:]
-    elr1 = np.sqrt(2.*0.1*9.1093837015E-31/1.602176634E-19)/BB
-    elr2 = np.sqrt(2.*1.*9.1093837015E-31/1.602176634E-19)/BB
-    elr3 = np.sqrt(2.*10.*9.1093837015E-31/1.602176634E-19)/BB
-    elr4 = np.sqrt(2.*100.*9.1093837015E-31/1.602176634E-19)/BB
+    me = 9.1093837015E-31
+    ee = 1.602176634E-19
+    Ke = np.array([0.1, 1.0, 10., 100.])
+    Ke = Ke*ee
+    cc = 2.99792458E+08
+    length = len(BB)
+    elr = np.zeros([4, length])
+    for i in range(length):
+        for j in range(4):
+            elr[j, i] = 1./cc/ee/BB[i]*np.sqrt(Ke[j]*(me*cc*cc+Ke[j]))
     fig = plt.figure()
-    plt.rcParams["font.size"] = 25
+    plt.rcParams["font.size"] = 20
     ax = fig.add_subplot(111, xlabel='S← MLT [degree] →N', ylabel='length [m]', yscale='log')   
     ax.plot(x, y[0, :], c='orange', label='ion Larmor radius - KAW')
     #ax.plot(x, y[1, :], c='deepskyblue', label='electron Larmor radius')
     #ax.plot(x, y[2, :], c='red', label='ion inertial legth')
     ax.plot(x, y[3, :], c='blue', label='electron inertial length - IAW')
-    ax.plot(x, elr1, c='indigo', label='electron Larmor radius - 100eV')
-    ax.plot(x, elr2, c='violet', label='electron Larmor radius - 1keV')
-    ax.plot(x, elr3, c='red', label='electron Larmor radius - 10keV')
-    ax.plot(x, elr4, c='green', label='electron Larmor radius - 100keV')
+    ax.plot(x, elr[0][:], c='indigo', label='electron Larmor radius - 100eV')
+    ax.plot(x, elr[1][:], c='violet', label='electron Larmor radius - 1keV')
+    ax.plot(x, elr[2][:], c='red', label='electron Larmor radius - 10keV')
+    ax.plot(x, elr[3][:], c='green', label='electron Larmor radius - 100keV')
     fig.suptitle('Larmor radius & inertial length')
     ax.grid()
     ax.legend()

942191603cf00f2aafac301a75fdc7fb6fbf29e6
santanka <mkrock1062@gmail.com>
2021-03-19 20:58:52 +0900

    pds_E_kaiの改訂に伴う1~6のデータと図の改訂

--- pds_kai/python_draw/codes/pds_E_kai_plot.py
+++ pds_kai/python_draw/codes/pds_E_kai_plot.py
@@ -8,9 +8,9 @@ Created on Wed Feb  3 16:18:06 2021
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.genfromtxt(r"/home/satanka/Documents/fort/pds_kai/pds_E_kai/pds_E_kai_4_all.csv", delimiter=',', unpack=True)
+data = np.genfromtxt(r"/home/satanka/Documents/fort/pds_kai/pds_E_kai/pds_E_kai_6_all.csv", delimiter=',', unpack=True)
 
-channel = 4
+channel = 6
 #1:静電ポテンシャル, 2:数密度, 3:Alfven速度, 4:圧力, 5:ベータ値, 6:Larmor半径&慣性長, 7:平行圧力
 
 if (channel == 1):

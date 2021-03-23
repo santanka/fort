f9adf60f90b019a7fe6e648025f2afb3fd34e7a6
santanka <mkrock1062@gmail.com>
2021-03-23 20:40:22 +0900

    pds_J_kai_1の作成

--- pds_kai/pds_J_kai/pds_J_kai_1.f90
+++ pds_kai/pds_J_kai/pds_J_kai_1.f90
@@ -1,13 +1,16 @@
-program pds_E_kai !地球
+program pds_J_kai !木星
 
 implicit none
 
 !mainの文字
-integer :: h, i, j, s, itn !do文用
-integer, parameter :: N = 255 !grid数
-integer, parameter :: Z = 500 !perp速度grid数
-integer, parameter :: MV = 128 !極小値となる座標
-integer, parameter :: CP = 0 !boundary以外の静電ポテンシャル無変化点
+integer :: h, i, j, s, itn, itn_all !do文用
+integer :: MV_itn !極小値ローラー作戦
+integer :: fin_num !100回更新なしでfinish
+integer :: trigger !finishコマンド(0:off, 1:on)
+integer, parameter :: N = 233 !grid数
+integer, parameter :: Z = 300 !perp速度grid数
+integer, parameter :: MV_fix = 117 !極小値となる座標
+integer, parameter :: CP = 117 !boundary以外の静電ポテンシャル無変化点
 double precision, parameter :: pi = 4.d0*atan(1.d0) !円周率
 character(len=80) :: dummy !読まない文字用
 
@@ -17,26 +20,26 @@ double precision, parameter :: cc = 2.99792458d+08 !光速
 double precision, parameter :: mu0 = 1.25663706143592d-06 !真空の透磁率
 double precision, parameter :: ep0 = 8.8541878128d-12 !真空の誘電率
 double precision, parameter :: GG = 6.6743015d-11 !万有引力定数
-double precision, parameter :: cc_rate = 1.d0 !光速の割合
+double precision, parameter :: cc_rate = 3.d-1 !光速の割合
 
 !惑星のデータ
-double precision, parameter :: Mdp = 7.8d+22 !惑星の磁気双極子モーメント
-double precision, parameter :: Mp = 5.9722d+24 !惑星の質量
-double precision, parameter :: Rp = 6.371d+06 !惑星の半径
-double precision, parameter :: omg = 7.292115024135739d-05 !惑星の自転角周波数
-double precision, parameter :: Lp = 4.d0 !対象の磁力線のL値
+double precision, parameter :: Mdp = 1.16d+27 !惑星の磁気双極子モーメント
+double precision, parameter :: Mp = 1.8982d+27 !惑星の質量
+double precision, parameter :: Rp = 7.1492d+07 !惑星の半径
+double precision, parameter :: omg = 1.75851813802955d-04 !惑星の自転角周波数
+double precision, parameter :: Lp = 5.84760d0 !対象の磁力線のL値
 double precision, parameter :: lamN = acos(sqrt((1.d0+5.d5/Rp)/Lp)) !boundary:NのMLT
 double precision, parameter :: lamS = -acos(sqrt((1.d0+5.d5/Rp)/Lp)) !boundary:SのMLT
 
 !衛星のデータ(設定しない場合は値を0に)
-double precision, parameter :: Ms = 0 !衛星の質量
-double precision, parameter :: Ls = 0 !衛星軌道のL値
+double precision, parameter :: Ms = 8.931938d22 !衛星の質量
+double precision, parameter :: Ls = 5.89856d0 !衛星軌道のL値
 
 !Boundary Condition(粒子のデータ)
-character(len=128) :: fileBC = 'pds_BC_E_kai_4.csv'
-integer, parameter :: kind = 12 !粒子種数
-integer, parameter :: nsc = 4 !boundary:Nでの数密度調整をする粒子
-integer, parameter :: ssc = 9 !boundary:Sでの数密度調整をする粒子
+character(len=128) :: fileBC = 'pds_BC_J_kai_1.csv'
+integer, parameter :: kind = 10 !粒子種数
+integer, parameter :: nsc = 1 !boundary:Nでの数密度調整をする粒子
+integer, parameter :: ssc = 3 !boundary:Sでの数密度調整をする粒子
 double precision, dimension(kind) :: sig !数密度
 double precision, dimension(kind) :: Tperp !boundaryでのperp温度[eV]
 double precision, dimension(kind) :: Tpara !boundaryでのpara温度[eV]
@@ -45,7 +48,7 @@ double precision, dimension(kind) :: mass !質量
 integer, dimension(kind) :: ijn !injection number
 
 !Initial Condition(初期静電ポテンシャル分布)
-character(len=128) :: fileIC = 'pds_IC_E_kai_4.csv'
+character(len=128) :: fileIC = 'pds_IC_J_kai_1.csv'
 double precision, dimension(N) :: Phi !静電ポテンシャル分布
 
 !場の設定
@@ -60,6 +63,7 @@ double precision, dimension(N) :: BB !磁束密度
 double precision, dimension(kind, Z) :: mu !第1断熱不変量 
 
 !iteration
+integer :: MV_N, MV_S !極小値(変数)
 double precision, dimension(3, N) :: Phih !差分込静電ポテンシャル
 double precision, dimension(3, kind) :: sigg !差分込数密度
 double precision, dimension(3, N, kind) :: UU !位置エネルギー
@@ -79,58 +83,31 @@ double precision, dimension(kind) :: nsig !数密度更新値
 
 
 !保存ファイル名
-integer, parameter :: channel = 1 !はじめから(1)orつづきから(2)
-character(len=128) :: fileresult = 'pds_E_kai_4_result.csv'
-72 format(1PE25.15E3, 18(',', 1PE25.15E3)) !kind+7
-character(len=128) :: filepote = 'pds_E_kai_4_potential.csv'
-82 format(1PE25.15E3, 15(',', 1PE25.15E3)) !kind+4
-character(len=128) :: filemin = 'pds_E_kai_4_min.csv'
+character(len=128) :: filetop = 'pds_J_kai_1_'
+character(len=128) :: fileresult = '_result.csv'
+72 format(1PE25.15E3, 16(',', 1PE25.15E3)) !kind+7
+character(len=128) :: filepote = '_potential.csv'
+82 format(1PE25.15E3, 13(',', 1PE25.15E3)) !kind+4
+character(len=128) :: filemin = '_min.csv'
 92 format(1PE25.15E3) !1
-character(len=128) :: filecheck = 'pds_E_kai_4_check.csv'
-62 format(1PE25.15E3, 19(',', 1PE25.15E3)) !kind+8
+character(len=128) :: filecheck = '_check.csv'
+character(len=128) :: fileresult_MV, filepote_MV, filemin_MV, filecheck_MV, file_MV
 52 format(1PE25.15E3, 4(',', 1PE25.15E3)) !5(double precision)
 42 format(I5)
 32 format(1PE25.15E3, 7(',', 1PE25.15E3)) !8(double precision)
 
 
-!/////データ抽出/////
-!BCの抽出
-open(30, file=fileBC, action="read", status="old")
-read(30, *) !1行読み飛ばし
-do s = 1, kind
- read(30, *) dummy, sig(s), Tperp(s), Tpara(s), chr(s), mass(s), ijn(s)
-enddo !s
-close(30)
-chr = chr*ee !次元を[C]に
-Tperp = Tperp*ee !次元を[J]に
-Tpara = Tpara*ee !次元を[J]に
-
-if(channel == 1) then
-  !ICの抽出
-  open(40, file=fileIC, action="read", status="old")
-  do i = 1, N
-   read(40, *) Phi(i)
-  enddo !i
-  close(40)
-  DDD = 0.d0
-  
- else if(channel == 2) then
-  !最低値更新
-  open(40, file=filemin)
-  read(40, *) DDD
-  do i = 1, N
-   read(40, *) lam(i), ss(i), BB(i), Phi(i), nd(1, i, :), rhod(1, i), rhop(1, i), cvg(1, i)
-  enddo !i
-  do s = 1, kind
-   read(40, *) sig(s)
-  enddo !s
-endif
-
 !/////場の設定/////
 
 !MLTの分割
 do i = 1, N
- lam(i) = lamS + (lamN-lamS)*dble(i-1)/dble(N-1)
+ if(i <= 41) then
+   lam(i) = lamS + (lamN-lamS)*dble(i-1)/dble(1600)
+  else if(i >= 42 .and. i <= 193) then
+   lam(i) = lamS + (lamN-lamS)*dble(i-41+4)/dble(160)
+  else if(i >= 194) then
+   lam(i) = lamS + (lamN-lamS)*dble(i-193+1560)/dble(1600)
+ endif
 enddo !i
 
 !惑星中心との距離
@@ -153,121 +130,166 @@ enddo !i
 BB = mu0*Mdp/4.d0/pi/(Lp*Rp)**3.d0 * sqrt(1.d0+3.d0*sin(lam)**2.d0) / cos(lam)**6.d0
 
 
-!断熱不変量
-call AI(N, Z, kind, cc_rate, cc, mass, BB, ijn, mu)
 
 
-!/////以下iteration/////
-itn = 0
+!/////極小値ローラー作戦/////
 
-do !itn
- itn = itn + 1
+itn_all = 0
+
+do MV_itn = 2, (N+1)/2-1
+ MV_S = MV_itn
+ MV_N = N - MV_itn + 1
+ fin_num = 0
+ trigger = 0
+
+ !/////データ抽出/////
+ !BCの抽出
+ open(30, file=fileBC, action="read", status="old")
+ read(30, *) !1行読み飛ばし
+ do s = 1, kind
+  read(30, *) dummy, sig(s), Tperp(s), Tpara(s), chr(s), mass(s), ijn(s)
+ enddo !s
+ close(30)
+ chr = chr*ee !次元を[C]に
+ Tperp = Tperp*ee !次元を[J]に
+ Tpara = Tpara*ee !次元を[J]に
+
+ !断熱不変量
+ call AI(N, Z, kind, cc_rate, cc, mass, BB, ijn, mu)
  
- !静電ポテンシャル差分
- call pepm(N, Phi, Phih)
- 
- !数密度差分
- call sigx(kind, nsc, ssc, sig, sigg)
- 
- !位置エネルギー
- call EP(N, kind, GG, Mp, mass, rr, omg, lam, Ms, rs, chr, Phih, UU)
- 
- !Accessibility
- call access(N, Z, kind, cc, cc_rate, mass, UU, mu, BB, ijn, amin, amax)
- 
- !積分
- call dense(N, Z, kind, Tperp, Tpara, sigg, mu, BB, ijn, amin, amax, nd)
- 
- !電荷密度(分布関数)
- call chde(N, kind, chr, nd, rhod, rhodp, rhodm)
- 
- !電荷密度(Poisson方程式)
- call chpo(N, ep0, ds, Phih, rhop)
- 
- !収束値
- call CV(N, rhod, rhodp, rhodm, rhop, cvg, cvn)
- print *, itn, cvn
- 
- !ファイル化処理
- open(50, file = fileresult)
+ !ICの抽出
+ open(40, file=fileIC, action="read", status="old")
  do i = 1, N
-  write(50, 72) lam(i), ss(i), BB(i), Phi(i), nd(1, i, :), rhod(1, i), rhop(1, i), cvg(1, i)
+  read(40, *) Phi(i)
  enddo !i
- close(50)
+ close(40)
+ DDD = 0.d0
+
+ !ファイル名作成
+ write(file_MV, *) lam(MV_N)
+ fileresult_MV = trim(adjustl(filetop))//'MVN='//trim(adjustl(file_MV))//trim(adjustl(fileresult))
+ filepote_MV = trim(adjustl(filetop))//'MVN='//trim(adjustl(file_MV))//trim(adjustl(filepote))
+ filemin_MV = trim(adjustl(filetop))//'MVN='//trim(adjustl(file_MV))//trim(adjustl(filemin))
+ filecheck_MV = trim(adjustl(filetop))//'MVN='//trim(adjustl(file_MV))//trim(adjustl(filecheck))
  
- open(60, file = filepote)
- do i = 1, N
-  write(60, 82) lam(i), ss(i), BB(i), Phi(i), UU(1, i, :)
- enddo !i
- close(60)
+ !/////以下iteration/////
+ itn = 0
  
- !最低収束値
- if(DDD >= cvn .or. DDD == 0.d0) then
-   DDD = cvn
-   print *, "cvn min", DDD
-   open(33, file = filemin)
-   write(33, 92) cvn
-   do i = 1, N
-    write(33, 72) lam(i), ss(i), BB(i), Phi(i), nd(1, i, :), rhod(1, i), rhop(1, i), cvg(1, i)
-   enddo !i
-   do s = 1, kind
-    write(33, 52) sig(s), Tperp(s)/ee, Tpara(s)/ee, chr(s)/ee, mass(s)
-   enddo !s
-   do s = 1, kind
-    write(33, 42) ijn(s)
-   enddo !s
-   close(33)
- endif
+ do !itn
+  itn = itn + 1
+  itn_all = itn_all + 1
  
- !収束チェック
- !if(cvn < 1.d-5) then
- !  print *, "finish"
- !  stop
- !endif
- 
- !Newton法(静電ポテンシャル)
- call NewtonPhi(N, MV, CP, Phih, cvg, nPhi)
- 
- !Newton法(数密度)
- call Newtonsig(N, kind, ijn, sigg, cvg, nsig)
- 
- open(70, file = filecheck)
- write(70, 32) sigg(:, ssc), nsig(ssc), nsig(ssc)-sigg(1, ssc), cvg(:, 1)
- do i = 2, N-1
-  write(70, 32) Phih(:, i), nPhi(i), nPhi(i)-Phih(1, i), cvg(:, i)
- enddo !i
- write(70, 32) sigg(:, nsc), nsig(nsc), nsig(nsc)-sigg(1, nsc), cvg(:, N)
- close(70)
- 
- !NaN, infinityチェック
- do i = 1, N
-  if(nPhi(i) /= nPhi(i) .or. nPhi(i) == nPhi(i)-1.d0) then
-    if(nPhi(i) /= nPhi(i)) then
-      print *, "NaN発生"
-     else if(nPhi(i) == nPhi(i)-1.d0) then
-      print *, "infinity発生"
-    endif
-    stop
+  !静電ポテンシャル差分
+  call pepm(N, Phi, Phih)
+  
+  !数密度差分
+  call sigx(kind, nsc, ssc, sig, sigg)
+   
+  !位置エネルギー
+  call EP(N, kind, GG, Mp, mass, rr, omg, lam, Ms, rs, chr, Phih, UU)
+  
+  !Accessibility
+  call access(N, Z, kind, cc, cc_rate, mass, UU, mu, BB, ijn, amin, amax)
+  
+  !積分
+  call dense(N, Z, kind, Tperp, Tpara, sigg, mu, BB, ijn, amin, amax, nd)
+  
+  !電荷密度(分布関数)
+  call chde(N, kind, chr, nd, rhod, rhodp, rhodm)
+  
+  !電荷密度(Poisson方程式)
+  call chpo(N, ep0, ds, Phih, rhop)
+  
+  !収束値
+  call CV(N, rhod, rhodp, rhodm, rhop, cvg, cvn)
+  print *, itn_all, MV_itn, itn, fin_num, cvn
+  
+  !ファイル化処理
+  !open(50, file = fileresult_MV)
+  !do i = 1, N
+  ! write(50, 72) lam(i), ss(i), BB(i), Phi(i), nd(1, i, :), rhod(1, i), rhop(1, i), cvg(1, i)
+  !enddo !i
+  !close(50)
+  
+  !open(60, file = filepote_MV)
+  !do i = 1, N
+  ! write(60, 82) lam(i), ss(i), BB(i), Phi(i), UU(1, i, :)
+  !enddo !i
+  !close(60)
+  
+  !最低収束値
+  if(DDD > cvn .or. DDD == 0.d0) then
+    DDD = cvn
+    fin_num = 0
+    print *, "cvn min", DDD
+    open(33, file = filemin_MV)
+    write(33, 92) cvn
+    do i = 1, N
+     write(33, 72) lam(i), ss(i), BB(i), Phi(i), nd(1, i, :), rhod(1, i), rhop(1, i), cvg(1, i)
+    enddo !i
+    do s = 1, kind
+     write(33, 52) sig(s), Tperp(s)/ee, Tpara(s)/ee, chr(s)/ee, mass(s)
+    enddo !s
+    do s = 1, kind
+     write(33, 42) ijn(s)
+    enddo !s
+    close(33)
+   else
+    fin_num = fin_num + 1
   endif
-  if(cvg(1, i) /= cvg(1, i) .or. cvg(1, i) == cvg(1, i)-1.d0) then
-    if(cvg(1, i) /= cvg(1, i)) then
-      print *, "NaN発生"
-     else if(cvg(1, i) == cvg(1, i)-1.d0) then
-      print *, "infinity発生"
-    endif
-    stop
+  
+  !収束チェック
+  if(fin_num >= 50) then
+    print *, "finish"
+    trigger = 1
   endif
- enddo !i
- 
- !更新
- Phi = nPhi
- sig = nsig
- 
- 
-enddo !itn
+  
+  !Newton法(静電ポテンシャル)
+  call NewtonPhi(N, MV_fix, MV_N, MV_S, CP, Phih, cvg, nPhi)
+  
+  !Newton法(数密度)
+  call Newtonsig(N, kind, ijn, sigg, cvg, nsig)
+  
+  open(70, file = filecheck_MV)
+  write(70, 32) sigg(:, ssc), nsig(ssc), nsig(ssc)-sigg(1, ssc), cvg(:, 1)
+  do i = 2, N-1
+   write(70, 32) Phih(:, i), nPhi(i), nPhi(i)-Phih(1, i), cvg(:, i)
+  enddo !i
+  write(70, 32) sigg(:, nsc), nsig(nsc), nsig(nsc)-sigg(1, nsc), cvg(:, N)
+  close(70)
+  
+  !NaN, infinityチェック
+  do i = 1, N
+   if(nPhi(i) /= nPhi(i) .or. nPhi(i) == nPhi(i)-1.d0) then
+     if(nPhi(i) /= nPhi(i)) then
+       print *, "NaN発生"
+      else if(nPhi(i) == nPhi(i)-1.d0) then
+       print *, "infinity発生"
+     endif
+     trigger = 1
+   endif
+   if(cvg(1, i) /= cvg(1, i) .or. cvg(1, i) == cvg(1, i)-1.d0) then
+     if(cvg(1, i) /= cvg(1, i)) then
+       print *, "NaN発生"
+      else if(cvg(1, i) == cvg(1, i)-1.d0) then
+       print *, "infinity発生"
+     endif
+     trigger = 1
+   endif
+  enddo !i
+  
+  if(trigger == 1) exit
 
+  !更新
+  Phi = nPhi
+  sig = nsig
+  
+  
+ enddo !itn
+ 
+enddo !MV_itn
 
-end program pds_E_kai
+end program pds_J_kai
 
 
 
@@ -649,13 +671,13 @@ end subroutine CV
 
 
 !Newton法(静電ポテンシャル)
-subroutine NewtonPhi(N, MV, CP, Phih, cvg, nPhi)
+subroutine NewtonPhi(N, MV_fix, MV_N, MV_S, CP, Phih, cvg, nPhi)
  implicit none
- integer, intent(in) :: N, MV, CP
+ integer, intent(in) :: N, MV_fix, MV_N, MV_S, CP
  double precision, dimension(3, N), intent(in) :: Phih, cvg
  double precision, dimension(N), intent(out) :: nPhi
  
- integer :: i, k, search
+ integer :: i, k, search, mm, MV, MV_min, MV_max
  double precision :: CC, ser
  
  
@@ -663,67 +685,83 @@ subroutine NewtonPhi(N, MV, CP, Phih, cvg, nPhi)
  
  do i = 2, N-1
   CC = 0.d0
-  if((cvg(2, i) < cvg(1, i) .or. cvg(3, i) < cvg(1, i)) .and. i /= CP) then
-    if(cvg(2, i) == cvg(1, i)) then
-      CC = (Phih(3, i)-Phih(1, i))/(cvg(3, i)-cvg(1, i))*cvg(1, i)
-     else if(cvg(3, i) == cvg(1, i)) then
-      CC = (Phih(2, i)-Phih(1, i))/(cvg(2, i)-cvg(1, i))*cvg(1, i)
-     else
-      CC = ((Phih(3, i)-Phih(1, i))/(cvg(3, i)-cvg(1, i)) + (Phih(2, i)-Phih(1, i))/(cvg(2, i)-cvg(1, i)))/2.d0*cvg(1, i)
+  if(i /= CP) then
+    if(cvg(2, i) < cvg(1, i) .or. cvg(3, i) < cvg(1, i)) then
+      if(cvg(2, i) == cvg(1, i)) then
+        CC = (Phih(3, i)-Phih(1, i))/(cvg(3, i)-cvg(1, i))*cvg(1, i)
+       else if(cvg(3, i) == cvg(1, i)) then
+        CC = (Phih(2, i)-Phih(1, i))/(cvg(2, i)-cvg(1, i))*cvg(1, i)
+       else
+        CC = ((Phih(3, i)-Phih(1, i))/(cvg(3, i)-cvg(1, i)) + (Phih(2, i)-Phih(1, i))/(cvg(2, i)-cvg(1, i)))/2.d0*cvg(1, i)
+      endif
     endif
-  endif
-  if((abs(CC) < cvg(1, i) .and. cvg(1, i) <= 1.d-2) .and. i /= CP) then
-    nPhi(i) = Phih(1, i) - CC
-   else if(abs(CC) >= cvg(1, i) .and. cvg(1, i) <= 1.d-2) then
-    nPhi(i) = Phih(1, i) - CC/abs(CC)*cvg(1, i)
-   else if(abs(CC) < 1.d-2 .and. cvg(1, i) > 1.d-2) then
-    nPhi(i) = Phih(1, i) - CC
-   else if(abs(CC) >= 1.d-2 .and. cvg(1, i) > 1.d-2) then
-    nPhi(i) = Phih(1, i) - CC/abs(CC)*1.d-2
+    if(abs(CC) < cvg(1, i) .and. cvg(1, i) <= 5.d-1) then
+      nPhi(i) = Phih(1, i) - CC
+     else if(abs(CC) >= cvg(1, i) .and. cvg(1, i) <= 5.d-1) then
+      nPhi(i) = Phih(1, i) - CC/abs(CC)*cvg(1, i)
+     else if(abs(CC) < 5.d-1 .and. cvg(1, i) > 5.d-1) then
+      nPhi(i) = Phih(1, i) - CC
+     else if(abs(CC) >= 5.d-1 .and. cvg(1, i) > 5.d-1) then
+      nPhi(i) = Phih(1, i) - CC/abs(CC)*5.d-1
+    endif
   endif
  enddo !i
  
- search = 1
- if(MV > 1 .and. MV <= N) then
-   do while(search == 1)
-    search = 0
-    do k = 1, 5
-     do i = 1, MV-1
-      if(nPhi(i) < nPhi(i+1) .and. (i /= CP .and. i+1 /= CP)) then
-        if(search == 0) search = 1
-        if(i == 1) then
-          nPhi(i+1) = 2.d0*nPhi(i)-nPhi(i+1)
-         else
-          ser = nPhi(i)
-          nPhi(i) = nPhi(i+1)
-          nPhi(i+1) = ser
-        endif
-      endif
-     enddo !i
+ do mm = 1, 2
+  if(mm == 1) then
+    MV = MV_S
+    MV_min = 1
+    MV_max = MV_fix
+   else if(mm == 2) then
+    MV = MV_N
+    MV_min = MV_fix
+    MV_max = N
+  endif
+   
+  search = 1
+  if(MV > MV_min .and. MV <= MV_max) then
+    do while(search == 1)
+     search = 0
+     do k = 1, 5
+      do i = MV_min, MV-1
+       if(nPhi(i) < nPhi(i+1) .and. (i /= CP .and. i+1 /= CP)) then
+         if(search == 0) search = 1
+         if(i == MV_min) then
+           nPhi(i+1) = 2.d0*nPhi(i)-nPhi(i+1)
+          else
+           ser = nPhi(i)
+           nPhi(i) = nPhi(i+1)
+           nPhi(i+1) = ser
+         endif
+       endif
+      enddo !i
+     enddo
     enddo
-   enddo
- endif
- 
- search = 1
- if(MV < N .and. MV >= 1) then
-   do while(search == 1)
-    search = 0
-    do k = 1, 5
-     do i = MV, N-1
-      if(nPhi(i+1) < nPhi(i) .and. (i /= CP .and. i+1 /= CP)) then
-        if(search == 0) search = 1
-        if(i == N-1) then
-          nPhi(i) = 2.d0*nPhi(i+1)-nPhi(i)
-         else
-          ser = nPhi(i)
-          nPhi(i) = nPhi(i+1)
-          nPhi(i+1) = ser
-        endif
-      endif
-     enddo !i
-    enddo !k
-   enddo
- endif
+  endif
+  
+  search = 1
+  if(MV < MV_max .and. MV >= MV_min) then
+    do while(search == 1)
+     search = 0
+     do k = 1, 5
+      do i = MV, MV_max-1
+       if(nPhi(i+1) < nPhi(i) .and. (i /= CP .and. i+1 /= CP)) then
+         if(search == 0) search = 1
+         if(i == MV_max-1) then
+           nPhi(i) = 2.d0*nPhi(i+1)-nPhi(i)
+          else
+           ser = nPhi(i)
+           nPhi(i) = nPhi(i+1)
+           nPhi(i+1) = ser
+         endif
+       endif
+      enddo !i
+     enddo !k
+    enddo
+  endif
+ enddo !mm
+
+ nPhi(CP) = Phih(1, CP)
  
  return
  
